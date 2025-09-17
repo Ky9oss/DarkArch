36c3307b1557f7874bad75b181351873e66a733e
Ky9oss <bb3343956556@163.com>
2025-09-18 00:32:14 +0800

    update

--- nvim/lua/before-quit.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd("QuitPre", {
-  callback = function()
-    if vim.fn.exists(":MinimapClose") == 2 then
-      pcall(vim.cmd, "silent! MinimapClose")
-    end
-  end,
-})
-
